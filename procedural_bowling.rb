1c5df9e26cc4c9a3e131dbfcb94260a2b0531403
Liam Sean Brady <liamseanbrady@gmail.com>
2014-10-08 17:26:47 +0100

    Add problem scope

---
+++ procedural_bowling.rb
@@ -0,0 +1,10 @@
+Problem scope:
+  Create a bowling game
+  Game has two players
+  Basic game has 10 frames
+  Each frame has two balls unless the first ball is a strike
+  If the ball is a spare on the 10th frame, you get an extra ball
+  If the ball is a strike on the 10th frame, you get two extra balls
+  If a player gets a spare then the score for that frame is 10 plus the next ball
+  If a player gets a strike then the score for that frame is 10 plus the next two balls
+
